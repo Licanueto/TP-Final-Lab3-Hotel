9f5c8253e3006cb0eb2b27265363eccad08b840f
Licanueto <licanueto@hotmail.com>
2018-06-16 18:20:57 -0300

    agrega obtenerCantidadDeDias(), devuelve una cantidad de dias como int
    Calcula la cantidad de días en un intervalo tomando un día menos, es decir de lunes a miercoles va a contar dos dias (que son los que se cobran en un hotel)

--- pruebas Maru/Reserva.java
+++ pruebas Maru/Reserva.java
@@ -138,7 +138,11 @@ public class Reserva {
 		saldo = monto - importe;
 	}
 	
-	
+	public int obtenerCantidadDeDias(PlainDate inicio, PlainDate fin) { // Calcula la cantidad de días en un intervalo tomando un día menos, es decir de lunes a miercoles va a contar dos dias (que son los que se cobran en un hotel)
+    		DateInterval intervalo = DateInterval.between(inicio, fin);
+    		int cantidadDeDias = (int)intervalo.getLengthInDays()-1; //resta 1 porque cuando entra un dia y se va al otro cuenta como un solo día
+    		return cantidadDeDias;
+    	}
 	
 	
 }
