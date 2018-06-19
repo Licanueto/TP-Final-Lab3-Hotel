47629ed8430a79ebc0c8c7b14aaf630cd700479c
Licanueto <licanueto@hotmail.com>
2018-06-19 20:34:45 -0300

    Faltaba import para que funcione DateInterval

--- Concerje.java
+++ Concerje.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 import net.time4j.PlainDate;
+import net.time4j.range.DateInterval;
 
 public class Concerje extends Usuario implements IAbmUsuario{
 	
