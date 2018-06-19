63aa2e9acefa3bc29433dd3e9aa09ff7c804ea49
MatiasTassara <matiastassara59@gmail.com>
2018-06-19 09:08:53 -0300

    Update Frigobar.java

--- Frigobar.java
+++ Frigobar.java
@@ -1,45 +1,43 @@
+package Clases;
+
+
+	
+import java.util.*;
+import java.io.*;
+
+
 
 public class Frigobar {
-	
-	 /**
-     * Hoy deciamos con Lisandro de agregarle distintos productos al frigobar asi se puede manejar mas claro, por
-     * ejemplo: private double cocacola = 24,50; y eso se puede ir manejando con un metodo que sea consumir o algo
-     * por el estilo que setee el valor a 0 y lo acumule en saldoDebido, asi nos evitamos manejar 3 variables que estan
-     * bastante confusas.
-     * O sino directamente trabajar con 2 variables, 1 que sea el total de valor de los productos en el frigobar y lo
-     * que se consume se resta de esa variable y se acumula en saldoDebido directamente.
-     */
-
-    private double cantidad; //lo que vale lo que esta en el frigobar
-    private double precio;   //lo que se va consumiendo(se debe)
-    private double saldoDebido; //acumulador periodico para precio(lo que se debe cuando se recarga el frigobar)
-
-    public Frigobar(){
-        this.cantidad = 0;
-        this.precio = 0;
-        this.saldoDebido = 0;
-    }
-
-    public Frigobar(double cantidad){ //podemos crearlo ya lleno o no?? ver eso
-        this.cantidad = cantidad;
-        this.precio = 0;
-        this.saldoDebido = 0;
-    }
-
-    public double calularTotalApagar(){
-        if (precio == 0) {
-            return saldoDebido;
-        }
-        else{
-            saldoDebido += precio;
-            precio = 0;
-            return saldoDebido;
-        }
-    }
-    public void rellenarFrigobar(double cantidad){
-        saldoDebido += precio;
-        precio = 0;
-        this.cantidad = cantidad;
-    }
+		
+		private double saldo;
+		private static HashMap<String,Double> productos;
+		
+		public Frigobar() {
+			this.saldo = 0;
+			productos = new HashMap<>();
+			agregarAlInventario();
+		}
+		
+		
+		public double getSaldo() {
+			return saldo;
+		}
+		
+		public void agregarAlInventario() {
+			productos.put("Coca Cola",30.5);
+			productos.put("Agua mineral", 30.0);
+			productos.put("Chocolate",25.0);
+			productos.put("Cerveza",42.5);
+		}
+		
+		public void consumirProduto(String producto,byte cantidad) {
+			saldo += productos.get(producto) * cantidad;
+		}
+		
+		public void cancelarSaldo() {
+			saldo = 0;
+		}
 
 }
+
+
