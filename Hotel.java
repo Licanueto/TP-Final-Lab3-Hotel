ca285f64d21658b01089bd3850b47de3a50a80cd
MatiasTassara <matiastassara59@gmail.com>
2018-06-20 10:46:42 -0300

    Update Hotel.java

--- Hotel.java
+++ Hotel.java
@@ -4,20 +4,20 @@ public class Hotel {
 	private String nombre;
         private String direccion;
      	private String CUIT;
-        private BaseDeDatos<Concerje> concerjes;
+        /*private BaseDeDatos<Concerje> concerjes;
         private BaseDeDatos<Habitacion> habitaciones;
         private BaseDeDatos<Pasajero> pasajeros;
         private BaseDeDatos<Reserva> reservas;
-
+*/
      
      public Hotel(String nombre,String direccion,String CUIT){
         this.nombre = nombre;
         this.direccion = direccion;
 	this.CUIT = CUIT;
-        concerjes = new BaseDeDatos<>();
+  /*      concerjes = new BaseDeDatos<>();
         habitaciones = new BaseDeDatos<>();
         pasajeros = new BaseDeDatos<>();
-        reservas = new BaseDeDatos<>();
+        reservas = new BaseDeDatos<>();*/
     }
 
 }
