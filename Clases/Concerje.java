dddbd4770818d6d5666968b53a4db697d5be2418
MaruCagnoli <37384046+MaruCagnoli@users.noreply.github.com>
2018-06-20 21:26:34 -0300

    Update Concerje.java

--- Clases/Concerje.java
+++ Clases/Concerje.java
@@ -6,19 +6,48 @@ import java.util.List;
 import java.util.Scanner;
 
 import net.time4j.PlainDate;
-
+/**
+ * Esta clase contiene la información básica de un gerente de hotel, que forma parte de sus atributos,
+ * su comportamiento y todas las tareas que realiza. Esta clase hereda de la clase Usuario que integra tanto
+ * los atributos como los comportamientos comunes que hay entre los tres tipos de usuarios que tiene el 
+ * programa: Administrador / Conserje / Pasajero.
+ * Además implementa interfaces importantes para el buen desempeño de su función de concerje. 
+ * 
+ *
+ */
 public class Concerje extends Usuario implements IAbmUsuario, IAbmHabitacion {
 	
+	/**
+	 * Constructor de Concerje que recibe los atributos por parámetro y que serán
+	 * llamados en la superclase Usuario.
+	 * @param dni - simplemente su numero de documento de tipo String
+	 * @param nombre 
+	 * @param apellido
+	 */
+	
+	
 	public Concerje(String dni, String nombre, String apellido)
+	 
 	{
 		super(dni, nombre, apellido);
 	}
 	
+	/**
+	 * toString nos devuelve en un String con los datos de los atributos para poder
+	 * realizar diferentes cosas, entre ellas, imprimir por pantalla en caso de que
+	 * se desee hacerlo. Para ello llamamos a la super clase Usuario.
+	 */
+		
 	@Override
 	public String toString() {
 		
 		return super.toString();
 	}
+	/**
+	 * Método de la interfaz IAbmUsuario. Una de las responsabilidades
+	 * del concerje es dar de alta a los pasajeros para poder registrarlos en el sistema.
+	 */
+	
 	 @Override
 	    public void darDeAltaUsuario() {
 	        Scanner scanner = new Scanner(System.in);
@@ -63,6 +92,11 @@ public class Concerje extends Usuario implements IAbmUsuario, IAbmHabitacion {
 
 
 	    }
+	 
+	 /**
+	  * Método de la interfaz IAmbUsuario. Consiste en dar de baja a un pasajero en el sistema.
+	  * 
+	  */
 	 @Override
 	public void darDeBajaUsuario(String dni) {
 		 
@@ -81,6 +115,10 @@ public class Concerje extends Usuario implements IAbmUsuario, IAbmHabitacion {
 		
 		
 	}
+	 /**
+	  * Método de la interfaz IAmbUsuario que se utiliza cuando un pasajero fue anteriormente
+	  * dado de baja y se necesita darle el alta nuevamente en el sistema.
+	  */
 	 @Override
 	public void darDeAltaUsuario(String dni) {
 		
@@ -97,11 +135,23 @@ public class Concerje extends Usuario implements IAbmUsuario, IAbmHabitacion {
 			 r.getMessage();
 		 }
 	}
+	 /**
+	  * Método que permite verificar si en vez de recibir un objeto de tipo Pasajero
+	  * recibimos un NULL. En ese caso lanza una excepción que será capturada en el 
+	  * bloque invocante.
+	  * @param pasajerito - Es la Reserva que se envía para ser evaluada.
+	  */
 	 public void comprobarPasajero(Pasajero pasajerito)
 	 {
 		 if(pasajerito == null)
 			 throw new PasajeroNullException("El pasajero es inexistente");
 	 }
+	 /**
+	  * Método de la interfaz IAbmHabitacion. A traves del mismo, podemos habilitar
+	  * una determinada habitación cuyo número es pasado por parámetro y que
+	  * anteriormente fue deshabilitada por algun motivo que puede ser limpieza,
+	  * refacción, etc.
+	  */
 	 
 	 @Override
 	public void darAltaHab(String numeroHab) {
@@ -122,6 +172,10 @@ public class Concerje extends Usuario implements IAbmUsuario, IAbmHabitacion {
 		 }
 		
 	}
+	 /**
+	  * Método perteneciente a la interfaz IAbmHabitacion. Permite deshabilitar una determinada
+	  * habitación por un motivo que puede ser variado. 
+	  */
 	 @Override
 	public void darBajaHab(String numeroHab) {
 try {
@@ -140,6 +194,18 @@ try {
 		 }
 		
 	}
+	 /**
+	  * Cuando un pasajero desea realizar una reserva lo primero que se hace es consultar si 
+	  * en los dias que el pasajero desea hay habitaciones disponibles para la cantidad de personas
+	  * que quiere hospedarse en el hotel. El método confecciona un arreglo dinámico con los números
+	  * de habitaciones más convenientes de acuerdo a las fechas y a la capacidad de las habitaciones.
+	  * Dicho arreglo será enviado al Método Reservar, que es el encargado de concretar e instanciar
+	  * la reserva.
+	  * @param fechaIngreso - a partir de qué fecha desean hospedarse
+	  * @param fechaEgreso - hasta qué fecha desean quedarse en el hotel
+	  * @param cantPasajeros - variable que nos permite calcular la cantidad de habitaciones y de qué tipo.
+	  * @return
+	  */
 	
 	ArrayList<String> asignarHabitaciones(PlainDate fechaIngreso, PlainDate fechaEgreso, int cantPasajeros)
 	{
@@ -189,6 +255,15 @@ try {
 			return numerosHabitaciones;
 		}
 	}
+	/**
+	 * Método que pide información a Base de Datos acerca de la disponibilidad de espacio en el hotel
+	 * para contener la cantidad de pasajeros que desea hospedarse. En caso de no haber capacidad hotelera
+	 * arroja una excepción advirtiendo que esa operación no podrá ser realizada y la misma será capturada en
+	 * el bloque invocante.
+	 * @param cantPasajeros - cantidad de personas que desea hospedarse en el hotel.
+	 * @return - Devulve una variable booleana que permite evaluar en el método invocante.
+	 * @throws FaltaDisponibilidadException - Anuncio del tipo de excepción que se manejará.
+	 */
 	public boolean verificarDisponibilidad(int cantPasajeros)throws FaltaDisponibilidadException
 	{
 		boolean verifica = BaseDeDatos.hayCapacidad(cantPasajeros);
@@ -197,7 +272,15 @@ try {
 		return verifica;
 			
 	}
-	
+	/**
+	 * En este método se concreta la reserva. Se instancia y se incorpora a la Base de Datos para que posteriormente
+	 * el administrador la guarde en un archivo y nos quede así la información almacenada de la misma.
+	 * @param dni - documento de la persona que hará la reserva
+	 * @param fechaIngreso - fecha en la cual comenzará
+	 * @param fechaEgreso - fecha de finalización
+	 * @param numerosHabitaciones - ArrayList con el numero de habitaciones que debe ocupar para ese rango de fechas.
+	 * @return
+	 */
 	public int Reservar(String dni, PlainDate fechaIngreso, PlainDate fechaEgreso, ArrayList<String> numerosHabitaciones)
 	{
 		int numeroReserva = BaseDeDatos.obtenerUltimaReserva() + 1;
@@ -225,7 +308,12 @@ try {
 		return numeroReserva;
 				
 	}
-	
+	/**
+	 * Cuando el pasajero llega al hotel, realiza el Check in. El método comprueba si ese número de reserva
+	 * existe. Hace efectiva la reserva, atributo de la clase Reserva. Y luego la incorpora a Base de Datos.
+	 * @param numeroReserva - String con el que se envía la solicitud a Base de Datos para que se pueda
+	 * enviar y recibir la reserva.
+	 */
 	public void realizarCheckIn(int numeroReserva)
 	{
 		try{
@@ -244,6 +332,16 @@ try {
 		}
 		
 	}
+	/**
+	 * Este método pide la reserva a Base de Datos, comprueba si existe. De se así, pide a Base de Datos
+	 * el índice de la colección donde se encuentra. utiliza el método cobrar y descuenta el importe ingresado
+	 * por pantalla. Si el importe es suficiente, la deuda se cancelará. Posteriormente, vuelve los Frigobares
+	 * de cada una de las habitaciones que conforman la reserva a cero. De esta manera otro pasajero no pagará
+	 * saldos que no le correspondan. Y posteriormente se vuelve a cargar en Base de Datos en la misma posición 
+	 * donde se encontraba anteriormente.
+	 * @param numeroReserva
+	 * @param importe - double ingresado por el conserje por el importe percibido por parte del pasajero.
+	 */
 	public void realizarChekOut(int numeroReserva, double importe) //importe que ingresa en dinero el cliente
 	{
 		
@@ -279,6 +377,16 @@ try {
 		}
 		
 	}
+	
+	/**
+	 * Método utilizado por el método check out. Toma el importe aportado por el pasajero,
+	 * llama a métodos propios de la clase Reserva, calcula el monto adeudado y lo resta.
+	 * Si el importe fue suficiente confirma el pago total. Si el importe no alcanzó a cubrir
+	 * la deuda, quedará el registro en el historial de reserva de ese pasajero y no se confirmará
+	 * el pago total.
+	 * @param reserva 
+	 * @param importe - importe ingresado por teclado. Tarea del concerje de turno.
+	 */
 	public void cobrar(Reserva reserva, double importe) //recibimos el importe que paga y chekeamos si cancelamos
 	{
 
@@ -292,6 +400,13 @@ try {
 		
 		
 	}
+	/**
+	 * Método que permite cancelar una reserva. Recorre un ArrayList de numeros de habitaciones
+	 * y va llamando a Base de Datos para poder acceder a cada habitación. A su vez, como siguiente
+	 * paso, desocupa y deja libre/s la/s correspondiente/s habitación /es para que otro pasajero
+	 * pueda reservar en esas fechas. Se realiza el tratamiento de excepciones.
+	 * @param numeroReserva
+	 */
 	public void cancelarReserva(int numeroReserva)
 	{
 		
@@ -325,28 +440,57 @@ try {
 		
 	}
 			
-	
+	/**
+	 * Método que lanza una excepción si la reserva es nula por algun error en 
+	 * el ingreso de número de reserva. En caso de que no exista, avisa de esta 
+	 * excepción para que sea capturada en el módulo invocante.
+	 * @param reservita - reserva que recibe para comprobar.
+	 */
 	
 	public void comprobarReserva(Reserva reservita)
 	{
 		if(reservita == null)
 			throw new ReservaNulaException("No existe la reserva solicitada");
 	}
+	
+	/**
+	 * Método que comprueba si una habitación devuelta por la Base de Datos es nula.
+	 * En ese caso lanza una excepción para que el módulo invocante lo capture.
+	 * @param hab - habitación que debe ser evaluada.
+	 */
 	public void comprobarHabitacion(Habitacion hab)
 	{
 		if(hab == null)
 			throw new HabitacionNulaException("Habitacion no existente. ");
 	}
+	/**
+	 * Método que comprueba si el índice devuelto desde Base de Datos es igual
+	 * a -1, en ese caso, no se debe continuar con la operación que se está realizando.
+	 * Para ello lanza la excepción que nos indica ese problema en particular
+	 * y que será capturada en el módulo invocante.
+	 * @param indice - índice que tenemos que evaluar y que utilizamos en otro método y que
+	 * nos señala una posición en un arreglo.
+	 */
 	public void comprobarIndice(int indice)
 	{
 		if (indice == -1)
 			throw new IndiceIncorrectoException("Indice incorrecto en la busqueda de habitacion o de reserva");
 	}
+	/**
+	 * Metodo que devuelve un ArrayList de Strings que puede ser impreso por pantalla. El mismo nos proporciona
+	 * los números de las habitaciones que en el momento de la consulta están aptas de ser reservadas u ocupadas. 
+	 * @return - retorna el ArrayList de números de habitaciones en String.
+	 */
 	public ArrayList<String> verDisponibles()
 	{
 		ArrayList<String> disponibles = BaseDeDatos.buscarNumerosDeDisponibles();
 		return disponibles;
 	}
+	/**
+	 * Método que nos permite ver cuales son las habitaciones que no se pueden ocupar en ese momento dado de 
+	 * la consulta.
+	 * @return - Devuelve un ArrayList de String con los números de las habitaciones.
+	 */
 	public ArrayList<String> verOcupadas()
 	{
 		ArrayList<String> habitaciones = BaseDeDatos.buscarNumerosDeNoDisponibles();
