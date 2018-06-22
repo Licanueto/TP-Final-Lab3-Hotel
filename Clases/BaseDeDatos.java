4e24200f4d61fce14ebad48447f626fbc3f1ce2e
Licanueto <licanueto@hotmail.com>
2018-06-22 02:17:30 -0300

    Update BaseDeDatos.java

--- Clases/BaseDeDatos.java
+++ Clases/BaseDeDatos.java
@@ -9,7 +9,11 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
-
+/**
+ * FALTA LA DESCRIPCION DE LA CLASE !!!, los métodos ya están todos.
+ * @author Li
+ *
+ */
 public final class BaseDeDatos {
 
 
@@ -22,7 +26,10 @@ public final class BaseDeDatos {
     private static ArrayList<Reserva> reservas;
     private static HashMap<String,Concerje> concerjes;
     private static HashMap<String,Administrador> administradores;
-
+    
+    /**
+     * Constructor de BaseDeDatos. Notese que es privado y por ende la clase no puede ser instanciada.
+     */
     private BaseDeDatos(){
         habitaciones = new ArrayList<>();
         pasajeros = new HashMap<>();
@@ -31,9 +38,9 @@ public final class BaseDeDatos {
         administradores = new HashMap<>();
     }
 
-    
-
-
+    /**
+     * Lee un archivo "habitaciones.dat" y carga sus objetos - las habitaciones - en memoria, más específicamente a Base de datos.
+     */
     // ****VER LUEGO TRATAMIENTO DE EXCEPCIONES EN LOS METODOS QUE LEVANTAN LOS ARCHIVOS****
     public static void levantarHabitaciones() {
         ObjectInputStream lectura = null;
@@ -68,7 +75,9 @@ public final class BaseDeDatos {
         }
 
     }
-
+    /**
+     * Lee un archivo "pasajeros.dat" y carga sus objetos - los pasajeros - en memoria, más específicamente a Base de datos.
+     */
     public static void levantarPasajeros() {
         ObjectInputStream lectura = null;
         Pasajero auxiliar;
@@ -101,7 +110,9 @@ public final class BaseDeDatos {
             }
         }
     }
-
+    /**
+     * Lee un archivo "reservas.dat" y carga sus objetos - las reservas - en memoria, más específicamente a Base de datos.
+     */
     public static void levantarReservas() {
         ObjectInputStream lectura = null;
         Reserva auxiliar;
@@ -137,10 +148,19 @@ public final class BaseDeDatos {
 
 
     /////////// METODOS PARA HABITACIONES //////////////////////////////////////////////////////
-
+    
+    /**
+     * Agrega un objeto de tipo Habitacion a BaseDeDatos.
+     * @param habitacion objeto de tipo Habitacion.
+     */
     public static void agregarHabitacion(Habitacion habitacion){
         habitaciones.add(habitacion);
     }
+    /**
+     * Busca y devuelve una habitación en la base de datos por número.
+     * @param numHab Número de la habitación a buscar.
+     * @return Objeto de tipo Habitacion.
+     */
     public static Habitacion buscarPorNumero(String numHab){
         for(Habitacion habitacion: habitaciones){
             if(habitacion.getNumHabitacion().equals(numHab)){
@@ -149,21 +169,32 @@ public final class BaseDeDatos {
         }
         return null;
     }
-
+    /**
+     * Produce una lista de todas las habitaciones del hotel 
+     * @return Lista en forma de ArrayList de objetos de tipo Habitacion.
+     */
     public static ArrayList<Habitacion> obtenerHabitaciones(){
         return habitaciones;
     }
 
+    /**
+     * Produce una lista de las habitaciones que se encuentran disponibles para ser ocupadas en el presente momento.
+     * @return Lista en forma de ArrayList de objetos de tipo Habitacion.
+     */
     public static ArrayList<Habitacion> obtenerLibres(){   //devolver arraylist de string con las libres
         ArrayList<Habitacion> disponibles = new ArrayList<>();
         for(Habitacion habitacion: habitaciones){
-            if(habitacion.isDisponible() && !habitacion.isOcupada()){
+            if(habitacion.isDisponible()){
                 disponibles.add(habitacion);
             }
         }
         return disponibles;
     }
-
+    /**
+     * Realiza una recolección de todas las habitaciones de una determinada capacidad.
+     * @param numero Cantidad de pasajeros que las habitaciones buscadas pueden albergar como máximo.
+     * @return Lista en forma de ArrayList de objetos de tipo Habitacion.
+     */
     public static ArrayList<Habitacion> buscarPorCapacidad(byte numero){
         ArrayList<Habitacion> capacidadBuscada = new ArrayList<>();
         for(Habitacion habitacion: habitaciones){
@@ -173,6 +204,11 @@ public final class BaseDeDatos {
         }
         return capacidadBuscada;
     }
+    /**
+     * Produce y devuelve una lista de todas las habitaciones de determinado tipo que la base de datos posee.
+     * @param tipoHab Tipo de habitación de la cual se desea generar la lista.
+     * @return Lista en forma de ArrayList de objetos de tipo Habitacion.
+     */
     public static ArrayList<Habitacion> buscarPorTipo(String tipoHab){
         ArrayList<Habitacion> tipoBuscado = new ArrayList<>();
         for(Habitacion habitacion: habitaciones){
@@ -182,6 +218,11 @@ public final class BaseDeDatos {
         }
         return tipoBuscado;
     }
+    /**
+     * Genera y devuelve una lista de las habitaciones cuyo precio por noche sea menor o igual a aquel recibido por parámetro.
+     * @param precioMax Cota máxima de precio para realizar la lista.
+     * @return Lista en forma de ArrayList de objetos de tipo Habitacion.
+     */
     public static ArrayList<Habitacion> buscarPrecioMenorA(double precioMax){
         ArrayList<Habitacion> precioBuscado = new ArrayList<>();
         for(Habitacion habitacion: habitaciones){
@@ -191,6 +232,10 @@ public final class BaseDeDatos {
         }
         return precioBuscado;
     }
+    /**
+     * Produce una lista del estado actual de todas las habitaciones del hote.
+     * @return Lista en forma de ArrayList de Strings del estado de cada habitacion.
+     */
     public static ArrayList<String> listarHabitaciones(){
         ArrayList<String> listaHabitaciones = new ArrayList<>();
         for(Habitacion habitacion : habitaciones){
@@ -198,7 +243,10 @@ public final class BaseDeDatos {
         }
         return listaHabitaciones;
     }
-
+    /**
+     * Elimina una habitación de la Base de datos a partir de su número de habitación.
+     * @param numero Número en forma de String de la habitación a ser eliminada.
+     */
     public static void quitarHabitacion(String numero){
         for(int i = 0; i < habitaciones.size(); i++){
             if(habitaciones.get(i).getNumHabitacion().equalsIgnoreCase(numero)){
@@ -209,10 +257,18 @@ public final class BaseDeDatos {
             }
         }
     }
+    /**
+     * Elimina una habitación de la Base de datos a partir de una instancia de Habitacion
+     * @param habitacion Objeto de tipo Habitacion.
+     */
     public static void quitarHabitacion(Habitacion habitacion){
         habitaciones.remove(habitacion);
     }
-
+    /**
+     * Devuelve la precio por noche de una habitación determinada a partir de su número de habitación.
+     * @param numHabitacion Número en forma de String de la habitación para la cual se desea saber la tarifa.
+     * @return Precio de la habitación por noche. Devuelve '-1' si la habitación no existe.
+     */
     public static double obtenerTarifa(String numHabitacion) {
         for(int i = 0; i < habitaciones.size(); i++) {
             if(habitaciones.get(i).getNumHabitacion().equals(numHabitacion)) {
@@ -221,7 +277,11 @@ public final class BaseDeDatos {
         }
         return -1;
     }
-
+    /**
+     * Devuelve el saldo acumulado del frigobar de una determinada habitación para un momento dado.
+     * @param numHabitacion Número de habitación para la cual se desea averiguar el saldo.
+     * @return Saldo de la habitación. Devuelve '-1' si la habitación no existe.
+     */
     public static double obtenerSaldoFrigobar(String numHabitacion) {
         for(int i = 0; i < habitaciones.size(); i++) {
             if(habitaciones.get(i).getNumHabitacion().equals(numHabitacion)) {
@@ -230,6 +290,11 @@ public final class BaseDeDatos {
         }
         return -1;
     }
+    /**
+     * Determina si el hotel en su totalidad posee la capacidad suficiente para albergar una determinada cantidad de huespedes.
+     * @param cantPasajeros Cantidad de personas para la cual se desea saber si hay capacidad
+     * @return TRUE si hay capacidad, FALSE si no la hay.
+     */
     public static boolean hayCapacidad(int cantPasajeros) {
         int capacidadTotal = 0;
         for(int i = 0; i < habitaciones.size(); i++) {
@@ -240,17 +305,25 @@ public final class BaseDeDatos {
         }
         return false;
     }
-
+    /**
+     * Produce una lista con las habitaciones disponibles para ser ocupadas dada una estadía.
+     * @param ingreso Fecha del inicio de la estadía en formato PlainDate.
+     * @param egreso Fecha del fin de la estadía en formato PlainDate.
+     * @return Lista en formato ArrayList de objetos de tipo Habitacion.
+     */
     public static ArrayList<Habitacion> buscarAptas(PlainDate ingreso, PlainDate egreso){
         ArrayList<Habitacion> aptas = new ArrayList<>();
         for(int i = 0; i < habitaciones.size(); i++) {
-            if(habitaciones.get(i).isDisponible(ingreso,egreso) ) {
+            if(habitaciones.get(i).isDisponible(ingreso,egreso)) {
                 aptas.add(habitaciones.get(i));
             }
         }
         return aptas;
     }
-   
+   /**
+    * Produce una lista con el número de todas las habitaciones disponibles para ser ocupadas en el momento actual.
+    * @return Lista en forma de ArrayList de Strings.
+    */
     public static ArrayList<String> buscarNumerosDeDisponibles(){
         ArrayList<String> lista = new ArrayList<>();
         for(int i = 0; i < habitaciones.size();i++){
@@ -260,7 +333,10 @@ public final class BaseDeDatos {
         }
         return lista;
     }
-    
+    /**
+    * Produce una lista con el número de todas las habitaciones que no están disponibles para ser ocupadas en el momento actual.
+    * @return Lista en forma de ArrayList de Strings.
+     */
     public static ArrayList<String> buscarNumerosDeNoDisponibles(){
         ArrayList<String> lista = new ArrayList<>();
         for(int i = 0; i < habitaciones.size(); i++){
@@ -270,6 +346,12 @@ public final class BaseDeDatos {
         }
         return lista;
     }
+    /**
+     * Devuelve el número del indice de la lista de habitaciones de Base de datos en donde se encuentra una determinada habitación.
+     * Método util al ser usado de forma conjunta con agregarHabitacionAlIndice().
+     * @param numeroHab Número de la habitación para la cual se desea saber el indice.
+     * @return Indice en el ArrayList de habitaciones.
+     */
     public static int obtenerIndiceHabitacion(String numeroHab){
         int index = -1;
         for(int i = 0; i < habitaciones.size(); i++){
@@ -279,6 +361,12 @@ public final class BaseDeDatos {
         }
         return index;
     }
+    /**
+     * Agrega una habitación al ArrayList de habitaciones en Base de datos en un determinado indice.
+     * Método util al ser usado de forma conjunta con obtenerIndiceHabitacion().
+     * @param habitacion Objeto de tipo Habitacion a insertar.
+     * @param indice Indice en el que se desea insertar la habitación,
+     */
     public static void agregarHabitacionAlIndice(Habitacion habitacion,int indice){
         habitaciones.add(indice,habitacion );
     }
@@ -287,45 +375,77 @@ public final class BaseDeDatos {
 
 
     ///////////         METODOS DE PASAJEROS       ////////////////////////////////////////
+    /**
+     * Agrega un pasajero a la Base de datos.
+     * @param pasajero Objeto de tipo pasajero a agregar a la Base de datos.
+     */
     public static void agregarPasajero(Pasajero pasajero){
         pasajeros.put(pasajero.getDni(),pasajero);
 
     }
-
+    /**
+     * @return Devuelve un HashMao con todos los pasajeros cargados en la Base de datos del hotel.
+     */
     public static HashMap<String,Pasajero> obtenerPasajeros(){
         return pasajeros;
     }
-
+    /**
+     * Elimina un pasajero de la Base de datos de pasajeros a partir de su DNI.
+     * @param numDoc Número de DNI en forma de String del pasajero a eliminar.
+     */
     public static  void quitarPasajero(String numDoc){
         pasajeros.remove(numDoc);
     }
-
+    /**
+     * Busca y devuelve un pasajero en la Base de datos.
+     * @param dniPasajero DNI del pasajero que se busqueda.
+     * @return Objeto de tipo Pasajero. Devuelve null si no se lo encuentra.
+     */
     public static Pasajero buscaPasajeroporDni(String dniPasajero){
         Pasajero buscado = pasajeros.get(dniPasajero);
         return buscado;
     }
-
+    /**
+     * Busca un pasajero en la Base de datos y retorna su nombre y apellido en forma de String.
+     * @param dniPasajero DNI del pasajero del que se quiere saber el nombre y apellido.
+     * @return Nombre y apellido del pasajero en forma de String.
+     */
     public static String buscaNombreYapellido(String dniPasajero){
         String ApellidoYnombre = pasajeros.get(dniPasajero).getApellido() + " " +  pasajeros.get(dniPasajero).getNombre();
         return ApellidoYnombre;
 
     }
-
+    /**
+     * Busca un pasajero en la Base de datos mediante su DNI y devuelve su número de teléfono.
+     * @param dniPasajero DNI del pasajero del cual se quiere saber el número de teléfono.
+     * @return Número de teléfono en forma de String.
+     */
     public static String buscaTelefonoPorDni(String dniPasajero){
         String telefono = pasajeros.get(dniPasajero).getTelefonoMovil();
         return telefono;
     }
 
 //////////////        METODOS DE RESERVAS   ////////////////////////////
-
+    
+    /**
+     * Agrega una reserva a la Base de datos.
+     * @param elemento Objeto de tipo Reserva a agregar.
+     */
     public static void agregarReserva(Reserva elemento) {
         reservas.add(elemento);
     }
-
+    /**
+     * Produce una lista de todas las reservas en la base de datos
+     * @return Lista en forma de ArrayList de objetos de tipo Reserva.
+     */
     public static ArrayList<Reserva> obtenerReservas(){
         return reservas;
     }
-
+    /**
+     * Devuelve una reserva a partir del número de reserva.
+     * @param numero Número de la reserva a retornar.
+     * @return Objeto de tipo Reserva.
+     */
     public static Reserva obtenerReserva(int numero) {
         for(Reserva reserva: reservas){
             if(reserva.getNumeroReserva() == numero) {
@@ -335,10 +455,18 @@ public final class BaseDeDatos {
         return null;//capturar un nullpointerexception mas arriba, en el metodo que quiera modificar el retorno
         //o bien una excepcion custom lanzada desde este metodo
     }
-
+    /**
+     * Devuelve la última reserva que haya sido cargada a la base de datos.
+     * @return Objeto de tipo Reserva.
+     */
     public static int obtenerUltimaReserva() {
         return reservas.get(reservas.size() - 1).getNumeroReserva();
     }
+    /**
+     * Devuelve a partir de un número de reserva el indice en el que se la encuentra en el ArrayList de reservas en la base de datos.
+     * @param numeroReserva Número de reserva para la cual se desea saber el indice.
+     * @return
+     */
     public static int obtenerIndiceReserva(int numeroReserva){
         int index = -1;
         for(int i = 0; i < reservas.size(); i++){
@@ -348,6 +476,11 @@ public final class BaseDeDatos {
         }
         return index;
     }
+    /**
+     * Coloca una reserva en un indice determinado en la base de datos
+     * @param indice Indice en el que se desea colocar la reserva.
+     * @param reserva Objeto de tipo reserva a colocar.
+     */
     public static void agragarReservaAlIndice(int indice,Reserva reserva){
         reservas.add(indice,reserva );
     }
@@ -355,28 +488,51 @@ public final class BaseDeDatos {
 
 /////////// METODOS DE coNCERJES ////////////////////////
 
+    /**
+     * Agrega un concerje a la base de datos.
+     * @param concerje
+     */
     public static void agregarConcerje(Concerje concerje) {
         concerjes.put(concerje.getDni(),concerje);
     }
-
+    /**
+     * Devuelve un mapa con todos los concerjes cargados en la base de datos.
+     * @return HashMap con las instancias de Concerje.
+     */
     public static HashMap<String,Concerje> obtenerConcerjes(){
         return concerjes;
     }
-
+    /**
+     * Devuelve un concerje a partir de su DNI
+     * @param dni DNI del concerje a obtener.
+     * @return Objeto de tipo Concerje.
+     */
     public static Concerje obtenerUnConcerje(String dni) {
         Concerje aux = concerjes.get(dni);
         return aux;
     }
 ///////////ADMINISTRADORES////////////////////////
+    /**
+     * Agrega un Administrador a la base de datos.
+     * @param administrador Objeto de tipo Administrador a agregar.
+     */
     public static void agregarAdministrador(Administrador administrador){
         administradores.put(administrador.getDni(),administrador );
     }
-
+    /**
+     * Devuelve un mapa con todos los administradores cargados en la base de datos.
+     * @return HashMap con las instancias de Administrador.
+     */
     public static HashMap<String, Administrador> obtenerAdministradores() {
         return administradores;
     }
+    /**
+     * Devuelve un Administrador a partir de su DNI.
+     * @param dni DNI del Administrador a devolver.
+     * @return Objeto de tipo Administrador.
+     */
     public static Administrador obtenerAdministrador(String dni){
-        return administradores.get(dni);
+    	return administradores.get(dni);
     }
     //FALTA TERMINAR.............
 }
