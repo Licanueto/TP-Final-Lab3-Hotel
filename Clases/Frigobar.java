83ced8f608dd2fe75f15b2d0e88f4a4b3b2e5807
MatiasTassara <matiastassara59@gmail.com>
2018-06-25 15:07:24 -0300

    Update Frigobar.java

--- Clases/Frigobar.java
+++ Clases/Frigobar.java
@@ -4,11 +4,8 @@ import java.util.*;
 import java.io.*;
 
 /**
- * Esta clase define los comportamientos básicos de un frigobar de la habitación de un hotel, lugar en donde el "Frigobar" se encuentra enmarcado.
- * El frigobar posee solo dos atributos:
- * @param saldo El saldo acumulado del frigobar. Este inicia en cero al comienzo de la estadía, acumula lo que se vaya consumiendo y vuelve a cero cuando el pasajero abona.
- * @param productos Es un HashMap que contiene el los productos ofrecidos en el frigobar en forma de String y el precio de cada uno en forma de Double.
- *
+ * Esta clase define los comportamientos bÃ¡sicos de un frigobar de la habitaciÃ³n de un hotel, lugar en donde el "Frigobar" se encuentra enmarcado.
+ * El frigobar posee solo dos atributos: Double saldo, Hashmap<String,Double> productos (lista de productos)
  */
 public class Frigobar implements Serializable{
 
@@ -39,7 +36,7 @@ public class Frigobar implements Serializable{
      * Agrega un listado harcodeado de tanto productos como sus respectivos valores al inventario
      */
     public void agregarAlInventario() {// Los productos se encuentran escritos con minuscula ya que en consumirProducto el
-        productos.put("coca cola",30.5);// parámetro recibido se interpreta en minuscula sin importar como haya sido ingresado
+        productos.put("coca cola",30.5);// parÃ¡metro recibido se interpreta en minuscula sin importar como haya sido ingresado
         productos.put("agua mineral", 30.0);
         productos.put("chocolate",25.0);
         productos.put("cerveza",42.5);
@@ -50,12 +47,12 @@ public class Frigobar implements Serializable{
      * @param cantidad Cantidad de unidades consumidas.
      */
     public void consumirProduto(String producto,byte cantidad) {
-        String productoConsumido = producto.toLowerCase(); // Lo convierte a minuscula para asegurarse de que esté bien escrito
+        String productoConsumido = producto.toLowerCase(); // Lo convierte a minuscula para asegurarse de que estÃ© bien escrito
         saldo += productos.get(productoConsumido) * (double) cantidad;
     }
     /**
-     * Vuelve el saldo del frigobar a cero, útil al momento de pagar.
-     * Nótese que este método no devuelve la cantidad saldada así que para mantener la información previamente se deberá ejecutar getSaldo() para mantener esa información.
+     * Vuelve el saldo del frigobar a cero, Ãºtil al momento de pagar.
+     * NÃ³tese que este mÃ©todo no devuelve la cantidad saldada asÃ­ que para mantener la informaciÃ³n previamente se deberÃ¡ ejecutar getSaldo() para mantener esa informaciÃ³n.
      */
     public void cancelarSaldo() {
         saldo = 0;
